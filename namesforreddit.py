ae178667d9895a25c01eabeb0b82d85650d27c7f
ASebastian27 <sebastianacujboaei2705@gmail.com>
2021-07-23 17:17:04 +0300

    Update!
    After revisiting this project, I've did some improvments on it. Now, it should run with less setup and auto-update using ChromeDriverManager.

--- namesforreddit.py
+++ namesforreddit.py
@@ -1,4 +1,6 @@
+from os.path import dirname
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 import random
@@ -6,9 +8,8 @@ import time
 import re
 import string
 import secrets
-
-driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
-# or '/usr/lib/chromium-browser/chromedriver' if you use chromium-chromedriver
+import os
+driver = webdriver.Chrome(ChromeDriverManager().install()) # USES CHROMEDRIVERMANAGER TO AUTO UPDATE CHROMEDRIVER
 
 # GENERATE PASSWORD
 alphabet = string.ascii_letters + string.digits
@@ -22,12 +23,16 @@ for char in string.punctuation:
     temp = temp.replace(char, '') #REMOVES ALL PUNCTUATION
 for char in string.digits:
     temp = temp.replace(char, '') #REMOVES SPACES
+temp = "".join(filter(lambda char: char in string.printable, temp)) #REMOVES NON ASCII CHARACTERS
 name = ''.join(temp.split())
 name = name[:random.randint(5,7)] #KEEPS 5 TO 7 LETTERS OF THE ORIGINAL STRING
 
 
-randomNumber = random.randint(10000,99999) #GENERATES RANDOM NUMBER
-text_file = open("namesforreddit.txt", "a")
+randomNumber = random.randint(10000,99999)
+
+dirname = os.path.dirname(__file__)
+text_file_path = os.path.join(dirname, 'namesforreddit.txt')
+text_file = open(text_file_path, "a")
 text_file.write("USR: " + name + str(randomNumber) + " PWD: " + password) #OUTPUTS NAME AND NUMBER
 text_file.write("\n")
 text_file.close()
@@ -40,16 +45,9 @@ time.sleep(1)
 driver.get('https://www.reddit.com/register/')
 driver.find_element_by_id('regEmail').send_keys('mail@mail.mail')
 time.sleep(1)
-driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/form/div[1]/fieldset[3]/button').click()
+driver.find_element_by_xpath ("//button[contains(text(),'Continue')]").click()
 time.sleep(3)
 driver.find_element_by_id('regUsername').send_keys(finalName)
 driver.find_element_by_id('regPassword').send_keys(password)
-#driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div/div/span').click()
-ActionChains(driver).key_down(Keys.TAB).key_up(Keys.TAB).key_down(Keys.RETURN).key_up(Keys.RETURN).perform() #AUTOMATICALLY OPENS CAPTCHA
-# time.sleep(90) # 90 SECS TO MANUALLY SOLVE CAPTCHA
-# driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/form/div[3]/div[3]/button').click()
-# time.sleep(1)
-# driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div/form/div[2]/div[3]/button').click()
-# REDDIT ACCOUNT CREATION FINISHED
 
 # driver.close()
